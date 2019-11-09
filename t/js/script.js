ecb9b4322aea9e5e774e4ae02172dda54c2e397b
Phill <phill44phill@gmail.com>
2019-11-09 12:17:50 +0300

    dddd

--- t/js/script.js
+++ t/js/script.js
@@ -1,24 +1,71 @@
 $(document).ready(function(){
 	
-	$("#ff").on("submit",function(e){
-        console.log('work');
-        e.preventDefault();
-        $.ajax({
-            url:"send.php",
-            type:"POST",
-            data:{
-                name:$("#name").val()
-            },
-            error:function(){
-             console.log("Error");
-            },
-            success:function(res){
-                
-                console.log(res);
-            }
-        })
-    });
-	
+	var count=1;
 
+   $(".sub_menu").each(function(){
+    $(this).on("click",function(e){
+         e.preventDefault();
+    
+           var n_elem = $(this).next(".list-sub")
+         
+        
+        
+        
+        if(n_elem.is(":visible")==false){
+            
+            n_elem.slideDown(500);
+            $(this).addClass("amb");
+            $(this).removeClass("amt");
+            
+        }else{
+              $(this).addClass("amt");
+              $(this).removeClass("amb");
+              n_elem.slideUp(500);
+        }
+        
+        
+        
+        
+       });
+       
+       
+   }); 
+    
+    
+    $(".anim_list").each(function(){
+        $(this).on("click",function(){
+         
+        var n_elem = $(this).next(".list")
+         
+     
+        
+        
+        if(n_elem.is(":visible")==false){
+            
+            n_elem.slideDown(500);
+            $(this).addClass("ahb");
+            $(this).removeClass("aht");
+            
+        }else{
+              $(this).addClass("aht");
+              $(this).removeClass("ahb");
+              n_elem.slideUp(500);
+        }
+        
+        
+       });
+       
+        
+        
+    });
+    
+    
+    $(".btn").on("click",function(){
+        
+       $(".main-2").slideToggle(500) 
+    });
+    
+    
+    
 	
 });
