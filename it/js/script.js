02e985826c8f537b3523c47e268663743786c582
Phill <phill44phill@gmail.com>
2020-02-23 23:14:27 +0300

    yes

--- it/js/script.js
+++ it/js/script.js
@@ -1,35 +1,40 @@
 $(document).ready(function(){
 	
-	
-	
 	$("#sw1").on("click",function(){
-         $(".block2").fadeOut(1000);
+        $("#sw2,#sw3").removeClass("active");
         $("#sw1").addClass("active");
-        $("#sw2").removeClass("active");
+        $(".w3,.w2").fadeOut(1000);
         setTimeout(function(){
-             $(".block1").fadeIn(1000);
+            $(".w1").fadeIn(1000);
         },1000);
+    });
     
-    });
-	$("#sw2").on("click",function(){
-         $(".block1").fadeOut(1000);
-          $("#sw2").addClass("active");
-        $("#sw1").removeClass("active");
-         setTimeout(function(){
-             $(".block2").fadeIn(1000);
+    
+    $("#sw2").on("click",function(){
+        $(".w1,.w3").fadeOut(1000);
+         $("#sw1,#sw3").removeClass("active");
+        $("#sw2").addClass("active");
+        setTimeout(function(){
+            $(".w2").fadeIn(1000);
         },1000);
     });
-	
-	
-	$(".item button").each(function(){
-        $(this).click(function(){
-            
-           $(this).prev(".des").slideDown(900);
-            console.log('Yes');
-        });
-        
-        
+    
+    $("#sw3").on("click",function(){
+        $("#sw2,#sw1").removeClass("active");
+        $("#sw3").addClass("active");
+        $(".w1,.w2").fadeOut(1000);
+        setTimeout(function(){
+            $(".w3").fadeIn(1000);
+        },1000);
     });
-	
-	
+    
+    
+    $(".c-o").each(function(){
+    
+        $(this).on("click",function(){
+            
+            $(".q-a .an").slideUp(1200);
+            $(this).parents('.q-block').next(".an").slideToggle(1200);
+        });
+    });
 });
